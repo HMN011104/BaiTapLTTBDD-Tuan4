54a15ee5b357ae92d2d26da3d9190228866a1392
HMN011104 <huynhminhnhut1112004@gmail.com>
2025-03-24 05:32:25 -0700

    Bài tập tuần 4

--- lib/screens/task_detail_screen.dart
+++ lib/screens/task_detail_screen.dart
@@ -16,7 +16,9 @@ class TaskDetailScreen extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return const Center(child: Text('Failed to load details:${snapshot.error}'));
+            return Center(
+               child: Text('Failed to load details: ${snapshot.error}'),
+            );
           } else if (snapshot.hasData) {
             final task = snapshot.data!;
             return Padding(
